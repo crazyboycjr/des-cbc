8944a4b6d5771375eb0b531f181b62450e74ffd1
crazyboycjr <crazyboycjr@gmail.com>
2016-10-14 10:54:02 +0800

    split des.h and common.h from des.cpp and add some helper function

---
+++ common.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+
+using namespace std;
+
+#define DEBUG
+
+// Every int type in this program are deal with BigEndian, 
+// which means the leftmost is the 0th bit and the rightmost
+// is the 63rd bit
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+#ifdef DEBUG
+#include <iostream>
+
+#define disp(...) __disp(#__VA_ARGS__, __VA_ARGS__)
+void __disp(const char *name, u64 x, int b = 8) {
+	printf("%s:", name);
+	for (int i = 0; i < 64; i++) {
+		if (i % b == 0) putchar('\n');
+		putchar((x << i >> 63) + '0');
+	}
+	puts("\n");
+}
+
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+const char *nextok(const char *__restrict s) {
+	const char *e = s + strlen(s);
+	int top = 0;
+	for (; s < e; s++) {
+		top += *s == '(' || *s == '[' || *s == '{';
+		top -= *s == ')' || *s == ']' || *s == '}';
+		if (!top && *s == ',') return s;
+	}
+	return e;
+}
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+	cerr << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+	const char *comma = nextok(names + 1);
+	cerr.write(names, comma - names) << " : " << arg1 << " | ";
+	__f(comma + 1, args...);
+}
+#else
+#define trace(...)
+#endif
+
+#endif
+
